9dc3bd0e6c30a6ba322307dd39723b67d5a3c43b
root <root@Ty-Laptop.localdomain>
2022-12-06 01:01:44 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  function hasTargetSum(array, target) {
+    const seenNumbers = {};
+    for (const number of array) {
+      const complement = target - number;
+      console.log(seenNumbers)
+      if (seenNumbers[complement]) return true;
+      seenNumbers[number] = true;
+    }
+    return false;
+  }
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(1)
 */
 
 /* 
   Add your pseudocode here
+  -make an empty obj
+  -iterate through array
+  -assign the keys to the seen numbers, which are the values of the array
+  -get differencer between target number and argument number and assign varible
+  -compare key values and varible value.
+  -if equal return true
 */
 
 /*
   Add written explanation of your solution here
+   takes an empty object and iterates through an array. as it iterates through we take the target parameter and the current number
+  were passing through and subtract them. an easy way to understand the process is it takes the array and assigns the numbers to
+  keys. then the complement (target - number being passed from array) check the keys, and if the value of complement and the keys are 
+  equal, it returns true
 */
 
 // You can run `node index.js` to view these console logs
